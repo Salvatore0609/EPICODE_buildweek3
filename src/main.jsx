3360fa4bf0f38d322f8a7d2e8b04babce9eddfb9
Salvatore0609 <salvatoredesole1998@gmail.com>
2025-02-24 11:46:12 +0100

    start build

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
