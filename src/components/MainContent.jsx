862048619b22ee1f4181bfd50da3f96d7a4766e5
GennaroSavastano <tantormainondormopiu@gmail.com>
2025-02-25 16:05:52 +0100

    main

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,49 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+import ProfileSection from "./MainContent/ProfileSection";
+import ExperienceSection from "./MainContent/ExperienceSection";
+import EducationSection from "./MainContent/EducationSection";
+import SkillSection from "./MainContent/SkillsSection";
+import InterestSection from "./MainContent/InterestsSection";
+import ActivitySection from "./MainContent/ActivitySection";
+import ProfileSettings from "./MainContentSide/ProfileSettings";
+import MaybeYouKnow from "./MainContentSide/MaybeYouKnow";
+import Interestings from "./MainContentSide/Interestings";
+
+const MainContent = () => {
+  return (
+    <Container>
+      <Row className="gap-4 d-flex justify-content-center border-0 ">
+        <Col md={8}>
+          {/* PANNELLO PRINCIPALE */}
+          <ProfileSection />
+          {/* ANALISI */}
+          <ActivitySection />
+          {/* ESPERIENZE */}
+          <ExperienceSection />
+          {/* FORMAZIONE */}
+          <EducationSection />
+
+          {/* COMPETENZE */}
+          <SkillSection />
+          {/* INTERESSI */}
+          <InterestSection />
+        </Col>
+        {/* ----------- */}
+
+        <Col md={2}>
+          {/* SETTINGS PROFILO */}
+          <ProfileSettings />
+          {/* ALTRE VISUALIZZAZIONI */}
+
+          <MaybeYouKnow />
+
+          {/* POTREBBE INTERESSARTI */}
+          <Interestings />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default MainContent;
