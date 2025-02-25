7b9126c2b961c4f0ce00f8ff179094e490d5c789
Salvatore0609 <salvatoredesole1998@gmail.com>
2025-02-25 11:11:04 +0100

    m

---
+++ src/redux/action/profileAction.js
@@ -0,0 +1,32 @@
+const API_URL = "https://striveschool-api.herokuapp.com/api/profile/";
+const BEARER_TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2JjNTc1ZGU3MDMzNzAwMTUzMTZkYmEiLCJpYXQiOjE3NDAzOTYzODIsImV4cCI6MTc0MTYwNTk4Mn0.ONZKTuW8uMZfm7TTZUQUDzRq8jfZZmWwJ4vefV07-jY"; // Sostituisci con il tuo token
+
+export const fetchProfile = async (userId = "me") => {
+  const response = await fetch(`${API_URL}${userId}`, {
+    headers: { Authorization: `Bearer ${BEARER_TOKEN}` },
+  });
+
+  if (!response.ok) {
+    throw new Error("Errore durante il fetch del profilo");
+  }
+
+  return response.json();
+};
+
+export const updateProfile = async (profileData) => {
+  const response = await fetch(API_URL, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${BEARER_TOKEN}`,
+    },
+    body: JSON.stringify(profileData),
+  });
+
+  if (!response.ok) {
+    throw new Error("Errore durante l'aggiornamento del profilo");
+  }
+
+  return response.json();
+};
---
+++ src/redux/reducers/profileSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const profileSlice = createSlice({
+  name: "profile",
+  initialState: null,
+  reducers: {
+    setProfile: (state, action) => {
+      return action.payload;
+    },
+    updateProfile: (state, action) => {
+      return { ...state, ...action.payload };
+    },
+  },
+});
+
+export const { setProfile, updateProfile } = profileSlice.actions;
+export default profileSlice.reducer;
