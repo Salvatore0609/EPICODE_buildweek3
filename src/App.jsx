9b8b204214570adf1f42a1b23d20cb917317102a
Fabio-Bertolino <fabiobertix97@gmail.com>
2025-02-26 11:04:09 +0100

    d

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import TopBar from "./components/TopBar";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import MessageBar from "./components/MessageBar";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <TopBar />
-        <MainContent />
+        <Routes>
+          <Route path="/main-content" element={<MainContent />} />
+          <Route path="/" element={<Homepage />} />
+        </Routes>
         <Footer />
         <MessageBar />
       </BrowserRouter>
